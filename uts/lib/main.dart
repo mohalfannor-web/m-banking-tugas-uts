c5a255048bc5d6eb94a00a256dbd4bbad1d66fcc
unknown <mohalfannor@gmail.com>
2026-03-23 23:34:17 +0700

    first commit

--- uts/lib/main.dart
+++ uts/lib/main.dart
@@ -16,3 +16,43 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class WondrHomePage extends StatelessWidget {
+  const WondrHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          "wondr",
+          style: TextStyle(
+            color: Colors.orange,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 16, top: 12, bottom: 12),
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.shade300),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Row(
+              children: [
+                Icon(Icons.logout, size: 16, color: Colors.black),
+                SizedBox(width: 4),
+                Text(
+                  "Keluar",
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
